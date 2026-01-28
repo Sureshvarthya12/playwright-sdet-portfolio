8e8506e2e5398bb3b67aa648f242d09a25ce0b8b
SureshVarthya12 <sureshvarthya123@gmail.com>
2026-01-27 21:36:42 -0500

    added api test using playwright

---
+++ tests/apiuser.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test');
+
+test('API - Get single post', async ({ request }) => {
+    const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
+
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+
+    expect(body.id).toBe(1);
+    expect(body.userId).toBe(1);
+    expect(body.title.length).toBeGreaterThan(0);
+});
