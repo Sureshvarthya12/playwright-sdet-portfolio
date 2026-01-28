21647512d2416e224f8141b46f425710363b631e
SureshVarthya12 <sureshvarthya123@gmail.com>
2026-01-27 21:45:26 -0500

    added CI run playwright tests in github actions

--- tests/apiuser.spec.js
+++ tests/apiuser.spec.js
@@ -1,13 +1,13 @@
 const { test, expect } = require('@playwright/test');
 
 test('API - Get single post', async ({ request }) => {
-    const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
+    const response = await request.get('https://jsonplaceholder.typicode.com/posts/2');
 
     expect(response.status()).toBe(200);
 
     const body = await response.json();
 
-    expect(body.id).toBe(1);
+    expect(body.id).toBe(2);
     expect(body.userId).toBe(1);
     expect(body.title.length).toBeGreaterThan(0);
 });
